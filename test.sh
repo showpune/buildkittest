039d8b131dccd1885470a4a3f90c428901bf10ed
zhiyongli <iamlizhiyong@outlook.com>
2024-01-23 15:05:49 +0800

    rootless

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 docker build -t showpune/buildkit-test:simple . --push
 
 docker build -t showpune/buildkit-test:client . -f Dockerfile.client --push
+
+docker build -t showpune/buildkit-test:root . -f Dockerfileroot --push
