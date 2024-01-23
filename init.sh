579acaf5b3d200eb10cf280ccca32ccecececc1f
Zhiyong Li <iamlizhiyong@outlook.com>
2024-01-23 13:28:30 +0800

    Delete init.sh

--- init.sh
+++
@@ -1,4 +0,0 @@
-kubectl create secret docker-registry dockerhub-credentials \
-    --docker-username=showpune \
-    --docker-password=dckr_pat_yA7DXaWKbKnzBWCQy48zvqRJ6VI \
-    --docker-email=iamlizhiyong@outlook.com
